816b23bbfaefc4bb908ba464b2937da015a3461f
Marius <mariuner@stud.ntnu.no>
2023-04-20 13:31:43 +0200

    Started working on a handler for "Current percentage of renewables"

---
+++ handlers/currentPercentageHandler.go
@@ -0,0 +1,39 @@
+package handlers
+
+import (
+  "encoding/csv"
+  "fmt"
+  "os"
+  "net/http"
+  "strings"
+)
+
+
+// Dedicated handler for GET requests
+func HandleGetRequestForCurrentPercentage(w http.ResponseWriter, r *http.Request) {
+
+	// Get the country name from the request URL
+	//URLParts := strings.Split(r.URL.String(), "/")
+	//countryName := URLParts[??]
+
+
+	// Open the CSV file
+	fd, err := os.Open("renewable-share-energy.csv")
+	if err != nil {
+		fmt.Println("Error opening CSV file.")
+		fmt.Println(err)
+	}
+	fmt.Println("Successfully opened the CSV file.")
+
+	// Remember to close the file at the end of the program
+	defer fd.Close()
+
+	// Read the CSV file
+	fileReader := csv.NewReader(fd)
+	records, err := fileReader.ReadAll()
+	if err != nil {
+		fmt.Println("Error reading CSV file.")
+		fmt.Println(err)
+	}
+	fmt.Println(records)
+}
